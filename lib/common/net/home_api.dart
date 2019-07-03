01d1f1ffec423ce2de7679a65c6233b5e2b136fc
xingyao1015 <397573943@qq.com>
2019-07-03 16:07:54 +0800

    主页添加推荐项目列表

--- lib/common/net/home_api.dart
+++ lib/common/net/home_api.dart
@@ -2,17 +2,41 @@ import 'package:dio/dio.dart';
 
 import 'dio_manager.dart';
 import '../entity/banner_entity.dart';
+import '../entity/recommond_entity.dart';
 
-const _homeApi = {"banner": "banner/json"};
 
+class HomeApi{
 
-Future<BannerEntity> getBanner() async{
-  try{
-    Response response = await DioManager.instance().get(_homeApi['banner']);
-    print(response.data);
-    return BannerEntity.fromJson(response.data);
-  }catch(e){
-    print(e);
-    return e;
+  static const _homeApi = {
+    HomeApiKey.BANNER: "banner/json", //首页banner
+    HomeApiKey.RECOMMOND: "project/list/1/json?cid=294" //首页推荐项目
+  };
+
+  static Future<BannerEntity> getBanner() async {
+    try {
+      Response response = await DioManager.instance().get(_homeApi[HomeApiKey.BANNER]);
+      print(response.data);
+      return BannerEntity.fromJson(response.data);
+    } catch (e) {
+      print(e);
+      return e;
+    }
   }
+
+  static Future<RecommondEntity> getRecommond() async {
+    try {
+      Response response = await DioManager.instance().get(_homeApi[HomeApiKey.RECOMMOND]);
+      print(response.data);
+      return RecommondEntity.fromJson(response.data);
+    } catch (e) {
+      print(e);
+      return e;
+    }
+  }
+
+}
+
+class HomeApiKey {
+  static const BANNER = "banner";
+  static const RECOMMOND = "recommond";
 }
--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:flutter_wanandroid_test/common/net/home_api.dart';
 import 'package:flutter_wanandroid_test/common/entity/banner_entity.dart';
+import 'package:flutter_wanandroid_test/common/entity/recommond_entity.dart';
 import 'package:flutter_wanandroid_test/resources/resources.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,22 +13,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: EasyRefresh(
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              SwiperDiy(),
-            ],
-          ),
-
-          onRefresh: (){
-            return null;
-          },
-        ));
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          SwiperDiy(),
+          RecommendList(),
+        ],
+      ),
+      onRefresh: () {
+        setState(() {});
+        return null;
+      },
+    ));
   }
 }
 
@@ -48,12 +50,11 @@ class _SwiperDiyState extends State<SwiperDiy> {
           );
         }
       },
-      future: getBanner(),
+      future: HomeApi.getBanner(),
     );
   }
 
   Widget _initSwiper(BannerEntity banner) {
-
     return Container(
       height: dp(208),
       child: Swiper(
@@ -71,6 +72,103 @@ class _SwiperDiyState extends State<SwiperDiy> {
           );
         },
       ),
+    );
+  }
+}
+
+class RecommendList extends StatefulWidget {
+  @override
+  _RecommendListState createState() => _RecommendListState();
+}
+
+class _RecommendListState extends State<RecommendList> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      builder: (context, data) {
+        if (data.hasData) {
+          return Container(
+            child: _initItems(data.data),
+          );
+        } else {
+          return Container(
+            child: Text("没有数据哦"),
+          );
+        }
+      },
+      future: HomeApi.getRecommond(),
+    );
+  }
+
+  Widget _initItems(RecommondEntity recommond) {
+    List<Widget> items = recommond.data.datas.map((data) {
+      return Container(
+        width: double.infinity,
+        height: dp(170),
+        padding: EdgeInsets.all(dp(10)),
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            border: new Border(
+                bottom: new BorderSide(width: 0.33, color: Colors.black26))),
+        child: Flex(
+          direction: Axis.horizontal,
+          children: <Widget>[
+            Expanded(
+                child: Flex(
+              direction: Axis.vertical,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  data.title,
+                  style: TextStyle(fontSize: sp(14), color: Colors.black87),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(top: dp(10), right: dp(10)),
+                  child: Text(
+                    data.desc,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 5,
+                    softWrap: true,
+                    style: TextStyle(
+                      fontSize: sp(12),
+                      color: Colors.black54,
+                    ),
+                  ),
+                )),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.favorite_border),
+                    Container(
+                      margin: EdgeInsets.only(left: dp(10), right: dp(10)),
+                      child: Text(
+                        data.author,
+                        style:
+                            TextStyle(fontSize: sp(8), color: Colors.black45),
+                      ),
+                    ),
+                    Text(
+                      data.niceDate,
+                      style: TextStyle(fontSize: sp(8), color: Colors.black45),
+                    )
+                  ],
+                )
+              ],
+            )),
+            Image.network(
+              data.envelopePic,
+              fit: BoxFit.cover,
+              width: dp(80),
+              height: dp(150),
+            )
+          ],
+        ),
+      );
+    }).toList();
+
+    return Column(
+      children: items,
     );
   }
 }
